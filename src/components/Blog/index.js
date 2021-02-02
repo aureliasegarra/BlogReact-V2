e5f1505ec920123b82905d42da82a7d854ed475f
virginia segarra <virginiasegarra@MacBook-Pro-de-virginia.local>
2021-02-02 15:48:18 +0100

    get data from api with axios

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,6 +1,7 @@
 // Import npm
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 // Import Components
 import Header from 'src/components/Header';
@@ -13,29 +14,59 @@ import { getPostsByCategory } from 'src/selectors';
 
 // Import data
 import categoriesData from 'src/data/categories';
-import postsData from 'src/data/posts';
 import './style.scss';
 
-const Blog = () => (
-  <div>
-    <Header categories={categoriesData} />
-    <Switch>
-      {categoriesData.map((category) => {
-        const filteredPosts = getPostsByCategory(postsData, category.label);
-        return (
-          <Route
-            exact
-            key={category.label}
-            path={category.route}
-          >
-            <Posts posts={filteredPosts} />
-          </Route>
-        );
-      })}
-      <Route><NotFound /></Route>
-    </Switch>
-    <Footer />
-  </div>
-);
+const Blog = () => {
+  // preparation of the posts, which will be an empty array
+  const [posts, setPosts] = useState([]);
+  // preparation of the state for the state loading
+  const [loading, setLoading] = useState(false);
+
+  const loadPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get('https://oblog-sarah-maau.herokuapp.com/api/posts');
+      setPosts(response.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+    finally {
+      // Loading status off anyways
+      setLoading(false);
+    }
+  };
+
+  useEffect(loadPosts, []); // run loadPost on 1st render of the Blog component
+
+  return (
+    <div>
+      <Header categories={categoriesData} />
+      {loading && (
+        <div>Loading</div>
+      )}
+      {!loading && (
+      <Switch>
+        {categoriesData.map((category) => {
+          const filteredPosts = getPostsByCategory(posts, category.label);
+
+          return (
+            <Route
+              exact
+              key={category.label}
+              path={category.route}
+            >
+              <Posts posts={filteredPosts} />
+            </Route>
+          );
+        })}
+        <Route><NotFound /></Route>
+      </Switch>
+      )}
+      <Footer />
+    </div>
+  );
+};
 
 export default Blog;
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DOMPurify from 'dompurify';
 
-const Post = ({ title, category, excerpt }) => (
-  <article className="post">
-    <h2 className="post__title">{title}</h2>
-    <span className="post__category">{category}</span>
-    <p className="post__content">{excerpt}</p>
-  </article>
-);
+const Post = ({ title, category, excerpt }) => {
+  const createMarkup = () => ({
+    __html: DOMPurify.sanitize(excerpt, { ALLOWED_TAGS: ['em', 'strong'] }),
+  });
+
+  return (
+    <article className="post">
+      <h2 className="post__title">{title}</h2>
+      <span className="post__category">{category}</span>
+      <p className="post__content" dangerouslySetInnerHTML={createMarkup()} />
+    </article>
+  );
+};
 
 Post.propTypes = {
   title: PropTypes.string.isRequired,
